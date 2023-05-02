5598f53347a8f9e14d6c6c7cfdd0cf9f423b0573
İsmail Yarar <ismailyarar4@gmail.com>
2023-05-02 19:10:32 +0300

    Add paste image.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,27 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async header() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()
+          }
+        ]
+      }
+    ]
+  }
 }
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self vitals.vercel-insights.com';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';  
+`
 
 module.exports = nextConfig
--- src/components/Dropzone.tsx
+++ src/components/Dropzone.tsx
@@ -12,36 +12,37 @@ function Dropzone({
     const reader = new FileReader();
     reader.onloadend = () => {
       const imageDataUri = reader.result as string;
-      console.log({ imageDataUri });
+      // console.log({ imageDataUri });
       setImage(imageDataUri);
     };
     reader.readAsDataURL(file);
   }
 
-  // useEffect(() => {
-  //   function handlePaste(event: ClipboardEvent) {
-  //     const item = event.clipboardData?.items[0];
-
-  //     if (!item) {
-  //       return;
-  //     }
-
-  //     console.log(item);
-  //     if (item.kind === "file" && item.type.match("image/*")) {
-  //       const image = item.getAsFile();
-  //       if (image) {
-  //         setImage(image.toString());
-  //       }
-  //       console.log(image);
-  //     }
-  //   }
-
-  //   document.addEventListener("paste", handlePaste);
-
-  //   return () => {
-  //     document.removeEventListener("paste", handlePaste);
-  //   };
-  // }, []);
+  useEffect(() => {
+    function handlePaste(event: ClipboardEvent) {
+      const item = event.clipboardData?.items[0];
+      if (!item) {
+        return;
+      }
+      // console.log(item);
+      if (item.kind === "file" && item.type.match("image/*")) {
+        // console.log("matched");
+        const file = item.getAsFile();
+        if (file) {
+          const reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onload = () => {
+            const str = reader.result as string;
+            setImage(str);
+          };
+        }
+      }
+    }
+    document.addEventListener("paste", handlePaste);
+    return () => {
+      document.removeEventListener("paste", handlePaste);
+    };
+  }, []);
 
   return (
     <div className="rounded-md border-2 border-dashed p-4 py-8 relative flex justify-center items-center bg-slate-900 hover:bg-slate-800 hover:transition min-h-[20rem]">
