813edbc38589a0ff49064d0026fdfa17d9097547
İsmail Yarar <ismailyarar4@gmail.com>
2023-06-23 14:06:01 +0300

    Remove meta tag.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -63,9 +63,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-      </head>
       <body className={inter.className + " bg-gray-950 text-white"}>
         {children}
         <Analytics />
