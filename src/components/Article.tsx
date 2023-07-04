49bf4ad81c965a58dd97068eb8f09ab05a0adbf0
İsmail Yarar <ismailyarar4@gmail.com>
2023-07-04 16:16:51 +0300

    Add posts.

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -10,13 +10,15 @@ export default function Article({ post }: { post: Blog }) {
   });
   return (
     <Link href={`/blog/${post.slug}`}>
-      <article className='flex flex-col items-start justify-between gap-4 rounded-lg border p-4'>
-        <Image src={post.image} alt={post.title} width={1024} height={1024} />
-        <div className='flex w-full justify-between text-sm text-text-400'>
-          <span>{post.author}</span>
-          <time dateTime={post.date}>{date}</time>
+      <article className='grid h-full gap-4 rounded-lg border p-4'>
+        <div className='space-y-4'>
+          <Image src={post.image} alt={post.title} width={1024} height={1024} />
+          <div className='flex w-full justify-between text-sm text-text-400'>
+            <span>{post.author}</span>
+            <time dateTime={post.date}>{date}</time>
+          </div>
+          <h2 className='text-xl font-bold md:text-2xl'>{post.title}</h2>
         </div>
-        <h2 className='text-xl font-bold md:text-2xl'>{post.title}</h2>
         <p>{post.description}</p>
       </article>
     </Link>
